924656d99b5564cf8c88a357a56140da62d921a2
Davide Campagna <gdavidecampagna@gmail.com>
2019-09-18 19:09:59 +0100

    add fetch to client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,34 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookingService from './services/BookingService.js'
+import BookingsForm from './components/BookingsForm'
+import BookingGrid from './components/BookingGrid'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      bookings: []
+    }
+  },
   components: {
-    HelloWorld
+
+  },
+  methods: {
+    fetchData(){
+        BookingService.getBookings()
+        .then(bookings => this.bookings = bookings);
+    }
+  },
+  mounted() {
+    this.fetchData();
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ client/src/components/BookingsForm.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <form>
+    <input type="submit" value="Add Booking">
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: "booking-form",
+  data() {
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
